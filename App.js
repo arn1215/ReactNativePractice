65ce09bdfcb3c789547e379da7abbac643516c40
A_Naqvi <Ali.Naqvi.Dev@gmail.com>
2022-09-04 00:20:11 -0500

    changed data

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-n
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {faker} from '@faker-js/faker'
 
 
 
@@ -24,10 +25,11 @@ const TabNavigation = () => {
       headerShown: false
     }}
     >
-      <Tab.Screen name='Home' component={HomeScreen} options={{ tabBarIcon: () => <Icon name='home' size={26} color='white' /> }} />
+      <Tab.Screen name='Home' component={HomeScreen} options={{ tabBarIcon: () => <Icon name='home' size={32} color='white' /> }} />
       <Tab.Screen name='Projects' component={ProjectsScreen} options={{ tabBarIcon: () => <Icon name='pencil' size={24} color='white' /> }} />
       <Tab.Screen name='Messages' component={MessagesScreen} options={{ tabBarIcon: () => <Icon name='envelope' size={24} color='white' /> }} />
       <Tab.Screen name='Payments' component={PaymentsScreen} options={{ tabBarIcon: () => <Icon name='credit-card-alt' size={20} color='white' /> }} />
+      <Tab.Screen name='Bids' component={BidsScreen} options={{ tabBarIcon: () => <Icon name='money' size={25} color='white' /> }} />
     </Tab.Navigator>
   )
 }
@@ -37,20 +39,23 @@ const TabNavigation = () => {
 
 const HomeScreen = () => (
   <ScrollView style={{ backgroundColor: "#f3f3f5" }}>
-    <Heading>Trending Woofs</Heading>
+      <View style={{ margin: 10}}>
+        <Icon name="cog" size={24} color="grey" style={{alignSelf: 'flex-end'}}/>
+      </View>
+
     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
       <View style={{ height: 120, flexDirection: 'row', paddingRight: 10, paddingLeft: 10 }}>
-        {data.woofs.map(woof => {
+        {data.Categories.map(woof => {
           return (
-            <WoofCard key={woof.id} style={woofCardStyles.card} avatar={woof.avatar} name={woof.name} />
+            <Card key={woof.id} style={CardStyles.card} avatar={woof.avatar} name={woof.name} />
           )
         })}
       </View>
     </ScrollView>
-    <Heading>New Woof Posts</Heading>
+    <Heading>Popular Categories</Heading>
     {data.posts.map(post => {
       return (
-        <WoofPost
+        <Post
           key={post.id}
           image={post.image}
           title={post.title}
@@ -88,6 +93,16 @@ const PaymentsScreen = () => {
   )
 }
 
+const BidsScreen = () => {
+  return (
+    <View>
+      <Text>Bids</Text>
+    </View>
+  )
+}
+
+
+
 // Basic reusable components
 
 
@@ -120,41 +135,39 @@ const styles = StyleSheet.create({
 
 // App-specific components
 
-const WoofCard = (props) => (
-  <View style={woofCardStyles.card}>
+const Card = (props) => (
+  <View style={CardStyles.card}>
     <Avatar url={props.avatar} />
-    <Title style={woofCardStyles.title}>{props.name.toUpperCase()}</Title>
+    <Title style={CardStyles.title}>{props.name.toUpperCase()}</Title>
   </View>
 );
 
-const woofCardStyles = StyleSheet.create({
+const CardStyles = StyleSheet.create({
   card: {
     height: 110, width: 85, borderRadius: 12, borderWidth: 1,
     borderColor: '#e7e3eb', justifyContent: 'space-around',
-    alignItems: 'center', padding: 5, shadowColor: '#470000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 1,
-    elevation: 10,
+    alignItems: 'center', padding: 5, 
+    elevation: 3,
     marginLeft: 6, marginRight: 6, backgroundColor: 'white',
 
   },
   title: { marginVertical: 10 },
 });
 
-const WoofPost = (props) => (
-  <View style={woofPostStyles.layout}>
-    <Image source={{ uri: props.image }} style={woofPostStyles.image} />
-    <View style={woofPostStyles.content}>
-      <Text style={woofPostStyles.title}>{props.title}</Text>
-      <Text style={woofPostStyles.description} numberOfLines={2}>{props.description}</Text>
+const Post = (props) => (
+  <View style={PostStyles.layout}>
+    <Image source={{ uri: props.image }} style={PostStyles.image} />
+    <View style={PostStyles.content}>
+      <Text style={PostStyles.title}>{props.title.toUpperCase()}</Text>
+      <Text style={PostStyles.description} numberOfLines={2}>{props.description}</Text>
     </View>
   </View>
 );
 
-const woofPostStyles = StyleSheet.create({
+const PostStyles = StyleSheet.create({
   layout: { flexDirection: 'row', margin: 10, alignItems: "center" },
   image: { flex: 1, height: '100%', borderRadius: 10 },
-  content: { flex: 2, justifyContent: "center" },
+  content: { flex: 1, justifyContent: "center" },
   title: { fontWeight: 'bold', padding: 6, color: "indigo" },
   description: { padding: 6, color: "indigo" },
 });
@@ -172,27 +185,24 @@ export default App;
 
 // "Fake" API data to use in your app
 const data = {
-  woofs: [
+  Categories: [
     {
-      id: 'woof-1',
-      name: 'Rex',
+      name: 'Landscaping',
       avatar: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?auto=format&fit=crop&w=648&q=80',
-      caretaker: 'Victor Grabarczyk',
-      source: 'https://unsplash.com/photos/x5oPmHmY3kQ',
+      id: 1,
+
     },
     {
-      id: 'woof-2',
-      name: 'Ball',
+      id: 2,
+      name: 'Plumbing',
       avatar: 'https://images.unsplash.com/photo-1585584114963-503344a119b0?auto=format&fit=crop&h=64&q=80',
-      caretaker: 'Tatiana Rodriguez',
-      source: 'https://unsplash.com/photos/J40C1k6Fut0',
+      
     },
     {
-      id: 'woof-3',
-      name: 'Happy',
+      id: 3,
+      name: 'Tattoos',
       avatar: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?auto=format&fit=crop&h=64&q=80',
-      caretaker: 'Marliese Streefland',
-      source: 'https://unsplash.com/photos/2l0CWTpcChI',
+
     },
     {
       id: 'woof-4',
@@ -212,31 +222,31 @@ const data = {
   posts: [
     {
       id: 'post-1',
-      image: 'https://images.unsplash.com/photo-1544568100-847a948585b9?auto=format&fit=crop&w=967&q=80',
-      title: 'Happy Woofs',
-      description: 'How to keep your woof health and happy. We\'ve asked some of the best experts out there.',
+      image: faker.image.business(1234, 2345, true),
+      title: 'Tattoos',
+      description: 'Got an itch for some ink? Book an artist today.',
       caretaker: 'Jamie Street',
       source: 'https://unsplash.com/photos/UtrE5DcgEyg',
     },
     {
       id: 'post-2',
-      image: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?auto=format&fit=crop&w=850&q=80',
-      title: 'Woofs & friends',
-      description: 'Best friends are important for humans, but also for dogs.',
+      image: faker.image.business(1234, 2345, true),
+      title: 'Landscaping',
+      description: 'Keep your lawn looking the best that it can.',
       caretaker: 'Krista Mangulsone',
       source: 'https://unsplash.com/photos/9gz3wfHr65U',
     },
     {
       id: 'post-3',
-      image: 'https://images.unsplash.com/photo-1558947530-cbcf6e9aeeae?auto=format&fit=crop&w=634&q=80',
-      title: 'Good Woofs',
-      description: 'A good woof is a woof that brings joy. Here are a few tips to let your woof behave.',
+      image: faker.image.business(1234, 2345, true),
+      title: 'Carpentry',
+      description: 'Find the best carpentry specialists around.',
       caretaker: 'Olia Nayda',
       source: 'https://unsplash.com/photos/f6v_Q0WXEK8',
     },
     {
       id: 'post-4',
-      image: 'https://images.unsplash.com/photo-1444212477490-ca407925329e?auto=format&fit=crop&w=1100&q=80',
+      image: faker.image.business(1234, 2345, true),
       title: 'Wild Woofs',
       description: 'In some parts of the world, wild woofs are very common. Learn how to interact in a nice way.',
       caretaker: 'Anoir Chafik',
@@ -244,7 +254,7 @@ const data = {
     },
     {
       id: 'post-5',
-      image: 'https://images.unsplash.com/photo-1567014543648-e4391c989aab?auto=format&fit=crop&w=1050&q=80',
+      image: faker.image.business(1234, 2345, true),
       title: 'Sleepy Woofs',
       description: 'Sleeping is just as important for woofs as it is for humans. What are the main things your woof dreams about.',
       caretaker: 'Max Singh',
@@ -252,7 +262,7 @@ const data = {
     },
     {
       id: 'post-6',
-      image: 'https://images.unsplash.com/photo-1524511751214-b0a384dd9afe?auto=format&fit=crop&w=967&q=80',
+      image: faker.image.business(300, 600, true),
       title: 'Exploring Woofs',
       description: 'Just sitting in one place is boring for most woofs. How do woofs explore the world?',
       caretaker: 'Jamie Street',
